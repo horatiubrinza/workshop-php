08689b8020bda6b84d6750ebbaab453c877ec153
Kristo Godari <kristo.godari@zitec.com>
2016-11-07 15:39:05 +0200

    Added PDO to project, also implemented save-user-data route to  handle data update.

--- web/index.php
+++ web/index.php
@@ -1,22 +1,40 @@
 <?php
 
+use IPC\Silex\Provider\PDOServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
 use ZWorkshop\EmotionAPI;
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+/**
+ * Register components.
+ */
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/../src/views',
 ));
 
+$app->register(new PDOServiceProvider(), [
+    'pdo.options' => [
+        'dsn' => 'mysql:host=localhost;dbname=php_workshop;charset=UTF8',
+        'username' => 'root',
+        'password' => '',
+        'options' => [],
+        'attributes' => [],
+    ],
+]);
+
 $app['debug'] = true;
 
-$app->get('/test-emotion-api', function(Request $request) {
+
+/**
+ * Test emotion api route
+ */
+$app->get('/test-emotion-api', function (Request $request) {
 
     $emotionApi = new EmotionAPI();
-    $imageUrl = 'D:/projects/workshop/source/upload/demo-emotions.jpg';
+    $imageUrl = 'D:\programs\wamp64\www\workshop-php-a-zitec\upload\demo-emotions.jpg';
 
     $emotions = $emotionApi->analyze($imageUrl);
     dump($emotions);
@@ -24,19 +42,79 @@ $app->get('/test-emotion-api', function(Request $request) {
 });
 
 
-$app->get('/admin', function() use ($app){
+/**
+ * Admin route
+ */
+$app->get('/admin', function () use ($app) {
     return $app['twig']->render('admin.html.twig', array(
         'title' => 'Admin Panel',
     ));
 });
 
-$app->post('/save-user-data', function (Request $request) {
-    /*
-     * Do something with the data
-     */
-    dump($request);
+
+/**
+ * Save user data route
+ */
+$app->post('/save-user-data', function (Request $request) use ($app) {
+
+    $dbConnection = $app['pdo.connection'];
+
+    if (isset($request->request)) {
+
+        $userName = 'kristo.godari';
+
+        $firstName = $request->request->get('frist_name');
+        $lastName = $request->request->get('last_name');
+        $email = $request->request->get('email');
+        $sex = $request->request->get('sex');
+        $userDescription = $request->request->get('user_description');
+        $programmingLanguages = implode('|', $request->request->get('programming_languages'));
+
+
+        $sql = "UPDATE `users` SET (`FirstName`, `LastName`, `Email`, `Sex`, `ProgramingLanguages`, `Description`)
+                  VALUES (:firstName, :lastName, :email, :sex, :programmingLanguages, :userDescription)
+                  WHERE `username` = :username;";
+
+        $params = [
+            ':firstName' => $firstName,
+            ':lastName' => $lastName,
+            ':email' => $email,
+            ':sex' => $sex,
+            ':programmingLanguages' => $programmingLanguages,
+            ':userDescription' => $userDescription,
+            ':username' => $userName,
+        ];
+
+        try {
+
+            $query = $dbConnection->prepare($sql);
+            $query->execute(array(
+                'firstName' => $firstName,
+                'lastName' => $lastName,
+                'email' => $email,
+                'sex' => $sex,
+                'programmingLanguages' => $programmingLanguages,
+                'userDescription' => $userDescription,
+                'username' => $userName,
+            ));
+
+            $redirectUrl = 'admin?success=0';
+            if($query->rowCount()){
+                $redirectUrl = 'admin?success=1';
+            }
+
+        }catch(Exception $e) {
+            $redirectUrl = 'admin?success=0';
+        }
+    }
+
+    return $app->redirect($redirectUrl);
 });
 
+
+/**
+ * Process Image route
+ */
 $app->post('/process-image', function (Request $request) {
     /*
      * Do something with the image
