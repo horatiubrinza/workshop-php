5d8d112536b05342abe11a0c9012c7bde3e5c030
Horatiu Brinza <horatiu.brinza@zitec.com>
2016-11-11 19:36:25 +0200

    updated test-data file

--- sql/test-data.sql
+++ sql/test-data.sql
@@ -3,11 +3,10 @@ USE `php_workshop`;
 
 # use http://www.sha1-online.com/ for password hashing
 # password = test123#
-INSERT INTO `users` (`FirstName`, `LastName`, `Email`, `Gender`, `ProgramingLanguages`, `Description`, `Username`, `Password`)
-VALUES ('Kristo', 'Godari', 'kristo.godari@zitec.com', 'MALE', 'PHP|JAVA|JAVASCRIPT|C#', 'Software Engineer', 'kristo.godari', 'dbbe0b9e0ffef386cbf307107379782883c0c50b');
+#INSERT INTO `users` (`FirstName`, `LastName`, `Email`, `Gender`, `ProgramingLanguages`, `Description`, `Username`, `Password`) VALUES ('Kristo', 'Godari', 'kristo.godari@zitec.com', 'MALE', 'PHP|JAVA|JAVASCRIPT|C#', 'Software Engineer', 'kristo.godari', 'dbbe0b9e0ffef386cbf307107379782883c0c50b');
 
 # password = test123#
-INSERT INTO `users` (`FirstName`, `LastName`, `Email`, `Gender`, `ProgramingLanguages`, `Description`, `Username`, `Password`)
-VALUES ('Horatiu', 'Brinza', 'horatiu.brinza@zitec.ro', 'MALE', 'PHP|JAVA|JAVASCRIPT|C#', 'Senior Software Engineer', 'horatiu.brinza', 'dbbe0b9e0ffef386cbf307107379782883c0c50b');
+#INSERT INTO `users` (`FirstName`, `LastName`, `Email`, `Gender`, `ProgramingLanguages`, `Description`, `Username`, `Password`) VALUES ('Horatiu', 'Brinza', 'horatiu.brinza@zitec.ro', 'MALE', 'PHP|JAVA|JAVASCRIPT|C#', 'Senior Software Engineer', 'horatiu.brinza', 'dbbe0b9e0ffef386cbf307107379782883c0c50b');
 
-
+# password = test123#
+INSERT INTO `users` (`FirstName`, `LastName`, `Email`, `Gender`, `ProgramingLanguages`, `Description`, `Username`, `Password`) VALUES ('Test', 'McDev', 'test@test.com', 'MALE', '', '', 'horatiu.brinza', 'test123#');
